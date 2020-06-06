fcbf2dd6b25fff12d378c16cef4319d8b302e67b
Renan Zulian <renanzulian@gmail.com>
2020-06-06 11:45:41 -0300

    Configurando o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.export = {
+  username: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  host: process.env.SQL_HOST,
+  dialect: process.env.SQL_DIALECT,
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
